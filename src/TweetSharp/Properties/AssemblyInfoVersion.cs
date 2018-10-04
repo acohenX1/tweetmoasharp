5597c4e9432171ad56b4d7b77cfa796813b0439d
Troy Willmot <yort@yortondotnet.com>
2018-10-04 20:10:59 +1300

    Updates for new release.

--- src/TweetSharp/Properties/AssemblyInfoVersion.cs
+++ src/TweetSharp/Properties/AssemblyInfoVersion.cs
@@ -9,5 +9,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 using System.Reflection;
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyFileVersion("5.1.0.0")]
